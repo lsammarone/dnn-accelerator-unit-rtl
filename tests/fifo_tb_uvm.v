c4fb8cbc50b4c976b7ad344d1f47e4df8a512973
Allen Pan <aifusenno1@gmail.com>
2022-01-13 13:14:07 +0000

    Created a more robust fifo_tb_uvm.v Renamed the old version to fifo_tb_uvm_simple.v

--- tests/fifo_tb_uvm.v
+++ tests/fifo_tb_uvm.v
@@ -1,7 +1,7 @@
 `define DATA_WIDTH (4)
 `define FIFO_DEPTH (3)
 `define COUNTER_WIDTH (1)
-`define TEST_LENGTH (15)
+`define TEST_LENGTH (100)
 
 interface fifo_if (input bit clk);
   logic rst_n;
@@ -14,50 +14,74 @@ interface fifo_if (input bit clk);
   logic clr;
 endinterface
 
+// Transaction Object
 class fifo_item;
-  rand bit [`DATA_WIDTH - 1 : 0] data;
+  rand bit [`DATA_WIDTH - 1 : 0] din;
+  rand bit enq;
+  rand bit deq;
+  bit [`DATA_WIDTH - 1 : 0] dout;
+
+  function void print(int id = "");
+    $display("T=%0t [transaction_id=%0d] din=%h enq=%d deq=%h dout=%h", $time, id, din, enq, deq, dout);
+  endfunction
 endclass;
 
+// Driver applies the generated stimulus to DUT
 class driver;
   virtual fifo_if vif;
   mailbox drv_mbx;
   
   task run();
     $display ("T=%0t [Write driver] Starting ...", $time);
-    @ (negedge vif.clk);
+    
+    // drive inputs at falling edges so that they are ready for the next rising edge
+
+    @ (negedge vif.clk);  // first negative edge is at 40ns
     forever begin
-      if (vif.full_n) begin
-        fifo_item transaction;
-        drv_mbx.get(transaction);
-        vif.enq = 1'b1;
-        vif.din = transaction.data;
-      end else begin
-        vif.enq = 1'b0;
+      fifo_item transaction;
+      drv_mbx.get(transaction);
+
+      // if full then cannot enqueue
+      if (!vif.full_n) begin
+        transaction.enq = 1'b0;
       end
-      if (vif.empty_n) begin
-        vif.deq = 1'b1;
-      end else begin
-        vif.deq = 1'b0;
+
+      // if empty then cannot dequeue
+      if (!vif.empty_n) begin
+        transaction.deq = 1'b0;
       end
+
+      // send the signals to interface
+      vif.din = transaction.din;
+      vif.enq = transaction.enq;
+      vif.deq = transaction.deq;
+
       @ (negedge vif.clk);
-    end       
+    end
   endtask
 endclass
 
+// Monitor captures data from interface and sends the transaction to scoreboard
 class monitor; 
   virtual fifo_if vif;
   mailbox scb_mbx; // mailbox connected to scoreboard
+
 
   task run();
     $display("T=%0t [Read monitor] Starting ...", $time);
     forever begin
       fifo_item transaction = new;
 
-      @ (posedge vif.clk);
-      if (vif.deq) begin
-        transaction.data = vif.dout;
-        scb_mbx.put(transaction);
-      end
+      // wait for rising edges so that synchronous signals are updated
+      @ (posedge vif.clk); // first positive edge is at 30ns
+      
+      transaction.din = vif.din;
+      transaction.dout = vif.dout;
+      transaction.enq = vif.enq;
+      transaction.deq = vif.deq;
+      
+      // send transaction to scoreboard
+      scb_mbx.put(transaction);
     end
   endtask
 endclass
@@ -66,20 +90,62 @@ class scoreboard;
   mailbox scb_mbx;
   int resp_id;
   reg [`DATA_WIDTH - 1 : 0] expected_data;
+  // create a "golden" fifo to track the correct behavior
+  reg [`DATA_WIDTH - 1 : 0] data_queue[0 : `FIFO_DEPTH - 1];
+  int get_idx = 0;
+  int put_idx = 0;
+  bit full = 0;
+  bit empty = 1;
 
   task run();
     forever begin
       fifo_item transaction;
       resp_id = 0;
-      expected_data = 0;
+
+      // the initial data is garbage since no stimulus applied yet, skip it
+      scb_mbx.get(transaction);  // at 30ns
+      // set the initial expected_data to the initial dout to pass the first comparison
+      expected_data = transaction.dout; 
+
       while(resp_id < `TEST_LENGTH) begin
-        scb_mbx.get(transaction);
-        if (transaction.data != expected_data) begin
-          $display("T=%0t [Scoreboard] Error! Received = %h, expected = %h", $time, transaction.data, expected_data);
+        scb_mbx.get(transaction);  // first mail arrives at 50ns
+        transaction.print(resp_id);
+
+        // Comparison
+        if (transaction.dout !== expected_data) begin
+          $display("T=%0t [Scoreboard] Error! Received = %h, expected = %h", $time, transaction.dout, expected_data);
         end else begin
-          $display("T=%0t [Scoreboard] Pass! Received = %h, expected = %h", $time, transaction.data, expected_data);
+          $display("T=%0t [Scoreboard] Pass! Received = %h, expected = %h", $time, transaction.dout, expected_data);
         end
-        expected_data = expected_data + 1;
+
+        // Update golden model
+        if (transaction.enq) begin
+          data_queue[put_idx] = transaction.din;
+          put_idx = (put_idx + 1) % `FIFO_DEPTH;
+          empty = 0; // since we just push something, cannot be empty
+          if (put_idx == get_idx) begin
+            full = 1;
+          end else begin
+            full = 0;
+          end
+        end
+
+        if (transaction.deq) begin
+          get_idx = (get_idx + 1) % `FIFO_DEPTH;
+          full = 0; // since we just pop something, cannot be full
+          if (put_idx == get_idx) begin
+            empty = 1;
+          end else begin
+            empty = 0;
+          end
+        end
+
+        // update expected_data for next transaction
+        // when FIFO is empty, dout always shows the last dequeued data (this is how fifo.v behaves)        
+        if (!empty) begin
+          expected_data = data_queue[get_idx];
+        end  
+
         resp_id = resp_id + 1;
       end
       $finish;
@@ -87,39 +153,41 @@ class scoreboard;
   endtask
 endclass
 
+// Environment class contains all verification components
 class env;
-  driver d0;
-  monitor m0;
-  scoreboard s0;
-  mailbox scb_mbx;
-  virtual fifo_if vif;
+  driver            d0;           
+  monitor           m0;           
+  scoreboard        s0;           
+  mailbox           scb_mbx;      // maillbox connecting monitor and scoreboard
+  virtual fifo_if   vif;          
 
   function new();
     d0 = new; 
     m0 = new;
     s0 = new;
     scb_mbx = new();
+    
+    m0.scb_mbx = scb_mbx;
+    s0.scb_mbx = scb_mbx;
   endfunction
 
   virtual task run();
     d0.vif = vif;
     m0.vif = vif;
-    m0.scb_mbx = scb_mbx;
-    s0.scb_mbx = scb_mbx;
 
     fork
       s0.run();
       d0.run();
       m0.run();    
     join_any 
-    endtask   
-endclass    
+  endtask   
+endclass
+
 
 class test;
   env e0;
   mailbox drv_mbx;
   int stim_id;
-  reg [`DATA_WIDTH : 0] test_data;
 
   function new();
     drv_mbx = new();
@@ -140,11 +208,9 @@ class test;
     $display ("T=%0t [Test] Starting write stimulus ...", $time);
 
     stim_id = 0;
-    test_data = 0;
     while(stim_id < `TEST_LENGTH) begin
       transaction = new;
-      transaction.data = test_data;
-      test_data = test_data + 1;
+      transaction.randomize();
       stim_id = stim_id + 1;
       drv_mbx.put(transaction);
     end
@@ -187,7 +253,7 @@ module fifo_tb;
         clk <= 0;
         rst_n <= 0;
         clr <= 0;
-        #40 rst_n <= 1;
+        #20 rst_n <= 1;
         t0 = new(); 
         t0.e0.vif = _if;
         t0.run();
